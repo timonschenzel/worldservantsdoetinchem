33d76116492575f6ec47275c8c2bc83c438ec231
Timon <timon@ubuntu.ubuntu-domain>
2019-08-29 22:21:06 +0200

    Add logos to footer.

--- storage/framework/cache/layout_templates.js
+++ storage/framework/cache/layout_templates.js
@@ -47,75 +47,82 @@ module.exports = {'app-layout': `<div>
 
 	<footer-layout></footer-layout>
 </div>`,'footer-layout': `<footer class="bg-white">
-	<div class="container mx-auto  px-8">
+	<div class="container mx-auto px-8 lg:flex">
 
-		<div class="w-full flex flex-col md:flex-row py-6">
+		<div class="flex-1 flex-col md:flex-row py-6">
+			<div class="lg:flex lg:flex-1">
+				<div class="flex-1 mb-6">
 				
-			<div class="flex-1 mb-6">
+					<a class="text-orange-600 no-underline hover:no-underline font-bold text-2xl lg:text-4xl" href="/"> 
+						<img src="images/worldservants-logo-doetinchem.png" class="-ml-8" style="height: 120px;" />
+					</a>
+				</div>
 			
-				<a class="text-orange-600 no-underline hover:no-underline font-bold text-2xl lg:text-4xl" href="/"> 
-					<img src="images/worldservants-logo-doetinchem.png" style="height: 120px;" />
-				</a>
+			
+				<div class="flex-1">
+					<p class="uppercase text-gray-500 md:mb-6">Project</p>
+					<ul class="list-reset mb-6">
+						<li class="mt-2 inline-block mr-2 md:block md:mr-0">
+							<a href="/ecuador" class="no-underline hover:underline text-gray-800 hover:text-orange-500">Ecuador</a>
+						</li>
+						<li class="mt-2 inline-block mr-2 md:block md:mr-0">
+							<a href="/cacao" class="no-underline hover:underline text-gray-800 hover:text-orange-500">Cacao</a>
+						</li>
+						<li class="mt-2 inline-block mr-2 md:block md:mr-0">
+							<a href="/fairtrade" class="no-underline hover:underline text-gray-800 hover:text-orange-500">Fairtrade</a>
+						</li>
+					</ul>
+				</div>
+				<div class="flex-1">
+					<p class="uppercase text-gray-500 md:mb-6">World Servants</p>
+					<ul class="list-reset mb-6">
+						<li class="mt-2 inline-block mr-2 md:block md:mr-0">
+							<a href="https://www.worldservants.nl" target="_blank" class="no-underline hover:underline text-gray-800 hover:text-orange-500">World Servants</a>
+						</li>
+						<li class="mt-2 inline-block mr-2 md:block md:mr-0">
+							<a href="https://www.worldservants.nl/doetinchem" target="_blank" class="no-underline hover:underline text-gray-800 hover:text-orange-500">Actieplatform</a>
+						</li>
+					</ul>
+				</div>
+				<div class="flex-1">
+					<p class="uppercase text-gray-500 md:mb-6">Social</p>
+					<ul class="list-reset mb-6">
+						<li class="mt-2 inline-block mr-2 md:block md:mr-0">
+							<a href="https://www.facebook.com/WorldServantsDoetinchem/" target="_blank" class="no-underline hover:underline text-gray-800 hover:text-orange-500">Facebook</a>
+						</li>
+					</ul>
+				</div>
+				<div class="flex-1">
+					<p class="uppercase text-gray-500 md:mb-6">Over ons</p>
+					<ul class="list-reset mb-6">
+						<li class="mt-2 inline-block mr-2 md:block md:mr-0">
+							<a href="/wie-wij-zijn" class="no-underline hover:underline text-gray-800 hover:text-orange-500">Wie wij zijn</a>
+						</li>
+						<li class="mt-2 inline-block mr-2 md:block md:mr-0">
+							<a href="/contact" class="no-underline hover:underline text-gray-800 hover:text-orange-500">Contact</a>
+						</li>
+					</ul>
+				</div>
 			</div>
-		
-		
-			<div class="flex-1">
-				<p class="uppercase text-gray-500 md:mb-6">Project</p>
+			<div class="text-black mt-4 lg:mt-24 uppercase text-gray-500">
+				&copy; World Sevants Doetinchem
+			</div>
+		</div>
+
+		<div class="lg:flex py-6">
+			<div>
 				<ul class="list-reset mb-6">
 					<li class="mt-2 inline-block mr-2 md:block md:mr-0">
-						<a href="/ecuador" class="no-underline hover:underline text-gray-800 hover:text-orange-500">Ecuador</a>
+						<a href="http://www.anbi.nl/" target="_blank" title="ANBI - Algemeen Nut Beogende Instelling" class="no-underline hover:underline text-gray-800 hover:text-orange-500"><img src="/images/anbi-logo.png" class="h-16 lg:h-12 -mb-2 lg:mb-0"></a>
 					</li>
 					<li class="mt-2 inline-block mr-2 md:block md:mr-0">
-						<a href="/cacao" class="no-underline hover:underline text-gray-800 hover:text-orange-500">Cacao</a>
+						<a href="http://www.cbf.nl/" target="_blank" title="CBF - Keur voor goede doelen" class="no-underline hover:underline text-gray-800 hover:text-orange-500"><img src="/images/cbf-logo.png" class="h-20 lg:h-20 -mb-4 lg:mb-0"></a>
 					</li>
 					<li class="mt-2 inline-block mr-2 md:block md:mr-0">
-						<a href="/fairtrade" class="no-underline hover:underline text-gray-800 hover:text-orange-500">Fairtrade</a>
-					</li>
-				</ul>
-			</div>
-			<div class="flex-1">
-				<p class="uppercase text-gray-500 md:mb-6">World Servants</p>
-				<ul class="list-reset mb-6">
-					<li class="mt-2 inline-block mr-2 md:block md:mr-0">
-						<a href="https://www.worldservants.nl" target="_blank" class="no-underline hover:underline text-gray-800 hover:text-orange-500">World Servants</a>
+						<a href="http://www.worldservants.nl/" target="_blank" title="World Servants" class="no-underline hover:underline text-gray-800 hover:text-orange-500"><img src="/images/world-servants-logo.png" class="h-16 lg:h-16 -mb-2 lg:mb-0 lg:ml-2"></a>
 					</li>
 					<li class="mt-2 inline-block mr-2 md:block md:mr-0">
-						<a href="https://www.worldservants.nl/doetinchem" target="_blank" class="no-underline hover:underline text-gray-800 hover:text-orange-500">Actieplatform</a>
-					</li>
-				</ul>
-			</div>
-			<div class="flex-1">
-				<p class="uppercase text-gray-500 md:mb-6">Social</p>
-				<ul class="list-reset mb-6">
-					<li class="mt-2 inline-block mr-2 md:block md:mr-0">
-						<a href="https://www.facebook.com/WorldServantsDoetinchem/" target="_blank" class="no-underline hover:underline text-gray-800 hover:text-orange-500">Facebook</a>
-					</li>
-				</ul>
-			</div>
-			<div class="flex-1">
-				<p class="uppercase text-gray-500 md:mb-6">Over ons</p>
-				<ul class="list-reset mb-6">
-					<li class="mt-2 inline-block mr-2 md:block md:mr-0">
-						<a href="/wie-wij-zijn" class="no-underline hover:underline text-gray-800 hover:text-orange-500">Wie wij zijn</a>
-					</li>
-					<li class="mt-2 inline-block mr-2 md:block md:mr-0">
-						<a href="/contact" class="no-underline hover:underline text-gray-800 hover:text-orange-500">Contact</a>
-					</li>
-				</ul>
-			</div>
-			<div class="flex-1">
-				<ul class="list-reset mb-6">
-					<li class="mt-2 inline-block mr-2 md:block md:mr-0">
-						<a href="http://www.anbi.nl/" target="_blank" title="ANBI - Algemeen Nut Beogende Instelling" class="no-underline hover:underline text-gray-800 hover:text-orange-500"><img src="/images/anbi-logo.png" style="height: 40px; margin-bottom: 10px;"></a>
-					</li>
-					<li class="mt-2 inline-block mr-2 md:block md:mr-0">
-						<a href="http://www.cbf.nl/" target="_blank" title="CBF - Keur voor goede doelen" class="no-underline hover:underline text-gray-800 hover:text-orange-500"><img src="/images/cbf-logo.png" style="height: 60px; margin-top: -10px; margin-bottom: 10px;"></a>
-					</li>
-					<li class="mt-2 inline-block mr-2 md:block md:mr-0">
-						<a href="http://www.worldservants.nl/" target="_blank" title="World Servants" class="no-underline hover:underline text-gray-800 hover:text-orange-500"><img style="height: 65px; margin-bottom: 10px;" src="/images/world-servants-logo.png"></a>
-					</li>
-					<li class="mt-2 inline-block mr-2 md:block md:mr-0">
-						<a href="http://www.pgdoetinchem.nl/" target="_blank" title="Protestantse Gemeente Doetinchem" class="no-underline hover:underline text-gray-800 hover:text-orange-500"><img style="height: 50px; margin-bottom: 10px;" src="/images/pgd-logo.png"></a>
+						<a href="http://www.pgdoetinchem.nl/" target="_blank" title="Protestantse Gemeente Doetinchem" class="no-underline hover:underline text-gray-800 hover:text-orange-500"><img src="/images/pgd-logo.png" class="h-16 lg:h-16 -mb-2 lg:mb-0 lg:mt-3"></a>
 					</li>
 				</ul>
 			</div>
