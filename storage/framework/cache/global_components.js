b654a9d17be089fec0ac93b65783faee3c8272c8
Timon <timon@ubuntu.ubuntu-domain>
2021-05-03 23:00:23 +0200

    Open in new window

--- storage/framework/cache/global_components.js
+++ storage/framework/cache/global_components.js
@@ -319,7 +319,7 @@ module.exports = {'n-case': {
 	template: `
 	<div>
 		<div class="bg-indigo-900 text-center py-4 lg:px-4">
-			<a href="http://www.worldservantsdoetinchem.nl/nieuws/werkvakantie-ecuador-afgelast-bouw-gaat-wel-door" title="Voor meer informatie over de verplaatsing klik hier">
+			<a href="http://www.worldservantsdoetinchem.nl/nieuws/werkvakantie-ecuador-afgelast-bouw-gaat-wel-door" target="_blank" title="Voor meer informatie over de verplaatsing klik hier">
 				<div class="p-2 bg-indigo-800 items-center text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex" role="alert">
 					<span class="text-lg font-semibold mx-2 text-left flex-auto">Werkvakantie 2021 Ecuador afgelast; bouw gaat wel door</span>
 					<svg class="fill-current opacity-75 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z"/></svg>
